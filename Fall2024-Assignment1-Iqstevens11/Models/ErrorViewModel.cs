abb8c56b599993f88459df244ecac715ee7ede07
zayhstevens <isaiahstevens17@gmail.com>
2024-09-11 12:22:22 -0500

    Adding the project

---
+++ Fall2024-Assignment1-Iqstevens11/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Fall2024_Assignment1_Iqstevens.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
